8797694edc7446000a8b7a2b07d6471fce441348
The Conqueror <vishal7920821202@gmail.com>
2025-01-04 23:41:56 +0530

    fix

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -19,7 +19,7 @@ const Login = () => {
 
     useEffect(() => {
         if (token) router.push("/");
-    }, []);
+    }, [token, router]);
 
     const loginUser = async (e) => {
         e.preventDefault();
